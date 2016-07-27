d9075e8e5eb8dc48b66caed09e93d2e29fe7bca3
hemalatha <avhemavinoth@gmail.com>
2016-07-27 15:46:18 +0530

    Update and rename gf.c to f.c

---
+++ f.c
@@ -0,0 +1,12 @@
+class SumOfNum
+{
+	public static void main(String args[])
+	{
+		int s= 0;
+		int n=10;
+		for(int i = 1;i <= n;i++)
+		{
+			s= s+ i;
+		}
+		System.out.println("The Sum Of "+n+" Numbers are:" + s);
+	}
