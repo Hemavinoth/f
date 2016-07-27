f2b092517782b0fce9b08cc179f5bea04a0bc394
hemalatha <avhemavinoth@gmail.com>
2016-07-27 15:55:24 +0530

    Update and rename f.java to f1.java

--- f1.java
+++ f1.java
@@ -4,11 +4,13 @@ public class Round{
 	public static void main(String[] args) {
      Scanner s = new Scanner(System.in);
      System.out.println("Enter a no ");
-     double num = s.nextDouble();
-     System.out.println(Math.round(num));
-	s.close();
-	}
+    int c, n;
+  for (c = 1; c <= 10; c++) 
+  {
+    n = rand() % 100 + 1;
+     }
 
+}
 }
 
     
